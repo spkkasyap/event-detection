54427d9d14c4c9ddac5ec1556bd6dbf2c74d376b
prathyush <prathyush.260@gmail.com>
2013-10-16 23:28:37 +0530

    Success

--- EventDetection/src/CountingWords.java
+++ EventDetection/src/CountingWords.java
@@ -1,9 +1,23 @@
+// This program is to find the frequency of the words in Tweets
+// @author prathyush
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class CountingWords {
-	
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{
+		BufferedReader br= new BufferedReader(new FileReader("DexterTweets"));
+		String tweet="";
+		while((tweet=br.readLine())!=null)
+		{
+			System.out.println(tweet);
+		}
 		System.out.println("Wait for some time");
+		br.close();
 	}
 
 }
