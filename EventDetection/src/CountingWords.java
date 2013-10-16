39761a2a7ec73ec7c97ecf2c8a1281d0c413583a
prathyush <prathyush.260@gmail.com>
2013-10-16 23:41:49 +0530

    Success

--- EventDetection/src/CountingWords.java
+++ EventDetection/src/CountingWords.java
@@ -2,8 +2,10 @@
 // @author prathyush
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 
@@ -11,14 +13,29 @@ public class CountingWords {
 	public static void main(String args[]) throws IOException
 	{
 		BufferedReader br= new BufferedReader(new FileReader("DexterTweets"));
+		BufferedWriter bw= new BufferedWriter(new FileWriter("DexterText"));
 		String tweet="";
 		int count=0;
 		while((tweet=br.readLine())!=null)
 		{
+			String[] tweetparts=tweet.split(",");
+			String text="";
+			if(tweetparts.length==5)
+				{
+					text=tweetparts[4];
+				}
+			else
+			{
+				System.out.println("Tweet contains comma inside the text");
+				for(int i=4;i<tweetparts.length;i++)
+					text+=tweetparts[i];
+			}
+			bw.write(text+"\n");
 			count++;
 		}
 		System.out.println("Number of tweets:"+count);
 		br.close();
+		bw.close();
 	}
 
 }
