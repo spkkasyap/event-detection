7708943ef91e025ab29887021488f568b36db5a9
prathyush <prathyush.260@gmail.com>
2013-10-23 21:05:00 +0530

    Success

--- EventDetection/src/DetectEvents.java
+++ EventDetection/src/DetectEvents.java
@@ -3,46 +3,47 @@
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.TreeMap;
 
 
-public class CountingWords {
-
+public class DetectEvents {
+	// Required Data Structures to store intermediate data from tweets
 	static TreeMap<Integer, String> transactionmap= new TreeMap<Integer, String>();
 	static TreeMap<String, ArrayList<Integer>> itemmapping= new TreeMap<String, ArrayList<Integer>>();
+	static HashSet<String> stopwords= new HashSet<String>(); 
+	// The main function
 	public static void main(String args[]) throws IOException
 	{
-		CountingWords object= new CountingWords();
-
+		DetectEvents object= new DetectEvents();
+		object.HashStopWords("stopwords_new.csv");
+		int count= object.ReadTweetsFile("DexterTweets");
 		System.out.println("Number of tweets:"+count);
-
 		object.WriteTransactionMap(count);
 		object.WriteItemMap(count);
-
 		System.out.println("size of words:"+itemmapping.size());
+	}
 	
-	}
-	public void WriteTransactionMap(int count) throws IOException
+	public void HashStopWords(String filename) throws IOException
 	{
-		BufferedWriter bw= new BufferedWriter(new FileWriter("TransactionMapping"));
-		Iterator<Integer> it = transactionmap.keySet().iterator();
-		while(it.hasNext())
-		{
-			int key= it.next();
-			bw.write(key+":"+transactionmap.get(key)+"\n");
-		}
-		bw.close();
+		BufferedReader br= new BufferedReader(new FileReader(filename));
+		String stopWords=br.readLine();
+		stopWords=stopWords.toLowerCase();
+		String[] words=stopWords.split(",");
+		for(String i : words)
+			stopwords.add(i);
+		br.close();
 	}
-
-	public void ReadTweetsFile(String filename) throws IOException
+	
+	// This function reads the Tweets input file and get the transaction map and item map
+	public int ReadTweetsFile(String filename) throws IOException
 	{
-		BufferedReader br= new BufferedReader(new FileReader("DexterTweets"));
+		BufferedReader br= new BufferedReader(new FileReader(filename));
 		String tweet="";
 		int count=0;
 		while((tweet=br.readLine())!=null)
@@ -62,7 +63,7 @@ public class CountingWords {
 			count++;
 			String[] words= text.split(" ");
 			int i=0;
-			while(i<words.length && words[i].length()>4 && !words[i].contains("@"))
+			while(i<words.length && words[i].length()>4 && !words[i].contains("@") && !stopwords.contains(words[i]))
 			{
 				if(itemmapping.containsKey(words[i]))
 				{
@@ -83,8 +84,21 @@ public class CountingWords {
 			transactionmap.put(count, text);
 		}
 		br.close();
+		return count;
 	}
-	
+	// Writes the transaction map into the output file
+	public void WriteTransactionMap(int count) throws IOException
+	{
+		BufferedWriter bw= new BufferedWriter(new FileWriter("TransactionMapping"));
+		Iterator<Integer> it = transactionmap.keySet().iterator();
+		while(it.hasNext())
+		{
+			int key= it.next();
+			bw.write(key+":"+transactionmap.get(key)+"\n");
+		}
+		bw.close();
+	}
+	// Writes the item map into the output file
 	public void WriteItemMap(int count) throws IOException
 	{
 		BufferedWriter bw1= new BufferedWriter(new FileWriter("ItemMapping"));
